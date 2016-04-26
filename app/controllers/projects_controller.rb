dcabfca559c25f81c242f4e23bdcfe1c09d01c6a
Emmanuel Crisóstomo <crisostomoemmanuel@gmail.com>
2016-04-26 05:06:05 -0400

    Projects can now be updated

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,31 +1,47 @@
 class ProjectsController < ApplicationController
-	def index
-		@projects = Project.all
-	end
+  def index
+    @projects = Project.all
+  end
 
-	def new
-		@project = Project.new
-	end
+  def new
+    @project = Project.new
+  end
 
-	def create
-		@project = Project.new(project_params)
-		
-		if @project.save
-			flash[:notice] = "Project has been created."
-			redirect_to @project
-		else
-			flash.now[:alert] = "Project has not been created."
-			render "new"
-		end
-	end
+  def create
+    @project = Project.new(project_params)
 
-	def show
-		@project = Project.find(params[:id])
-	end
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      flash.now[:alert] = "Project has not been created."
+      render "new"
+    end
+  end
 
-	private
+  def show
+    @project = Project.find(params[:id])
+  end
 
-	def project_params
-		params.require(:project).permit(:name, :description)
-	end
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash.now[:alert] = "Project has not been updated."
+      render "edit"
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,3 +1,5 @@
+require "rails_helper"
+
 RSpec.feature "Users can create new projects" do
 	before do
 		visit "/"
@@ -8,7 +10,7 @@ RSpec.feature "Users can create new projects" do
 		fill_in "Name", with: "Sublime Text 3"
 		fill_in "Description", with: "A text editor for everyone"
 		click_button "Create Project"
-		
+
 		expect(page).to have_content "Project has been created."
 
 		project = Project.find_by(name: "Sublime Text 3")
@@ -20,7 +22,7 @@ RSpec.feature "Users can create new projects" do
 
 	scenario "when providing invalid attributes" do
 		click_button "Create Project"
-		
+
 		expect(page).to have_content "Project has not been created."
 		expect(page).to have_content "Name can't be blank"
 	end
